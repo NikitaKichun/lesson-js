6d40457b60de9f24f1395d279b8e96ecdd7fd2f9
Nikita <nkicun89@gmail.com>
2024-09-05 20:18:11 +0400

    qi commit

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,7 +13,7 @@ const isVerifiedUser = true;
 const hasSpecialPermission = true;
 const hasTemporaryPass = false;
 
-let isAccess;
+let isAccess = true;
 
 // проверка условий доступа
 isAccess = (isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass);
